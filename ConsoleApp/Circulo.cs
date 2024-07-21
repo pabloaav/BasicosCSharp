5d3bbb96826e51366e83fd134efd0e866c86caa4
pabloaav <pablovicentin84@gmail.com>
2024-07-21 00:07:59 -0300

    ejemplo de encapsulacion y de propiedades autoimplementadas con setter validado

---
+++ ConsoleApp/Circulo.cs
@@ -0,0 +1,26 @@
+﻿class Circulo
+{
+   private double _radio;
+
+   public double Radio
+   {
+      get { return _radio; }
+      set
+      {
+         if (value >= 0)
+            _radio = value;
+         else
+            throw new ArgumentException("El radio no puede ser negativo");
+      }
+   }
+
+   public Circulo(double radio)
+   {
+      Radio = radio; // Usa el setter con validación
+   }
+
+   public double CalcularSuperficie()
+   {
+      return Math.PI * Math.Pow(Radio, 2);
+   }
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hola, Mundo!");
+﻿class Program
+{
+   static void Main(string[] args)
+   {
+      Console.WriteLine("Calculadora de Superficie de Círculo");
+      Console.Write("Ingrese el radio del círculo: ");
+
+      if (double.TryParse(Console.ReadLine(), out double radio))
+      {
+         try
+         {
+            Circulo miCirculo = new Circulo(radio);
+            double superficie = miCirculo.CalcularSuperficie();
+
+            Console.WriteLine($"La superficie del círculo con radio {radio} es: {superficie:F2}");
+         }
+         catch (ArgumentException e)
+         {
+            Console.WriteLine($"Error: {e.Message}");
+         }
+      }
+      else
+      {
+         Console.WriteLine("Error: Por favor ingrese un número válido para el radio.");
+      }
+
+      Console.WriteLine("Presione cualquier tecla para salir...");
+      Console.ReadKey();
+   }
+}
